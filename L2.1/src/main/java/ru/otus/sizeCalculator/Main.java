afc35eecb498de29f0a5ff6930a5875eeb98b958
Dmitry <dimazollo@gmail.com>
2017-04-14 20:24:11 +0300

    Add new tests

--- L2.1/src/main/java/ru/otus/sizeCalculator/Main.java
+++ L2.1/src/main/java/ru/otus/sizeCalculator/Main.java
@@ -19,23 +19,48 @@ public class Main {
 
         sampleSize = Integer.parseInt(bufferedReader.readLine());
 
-        Supplier<String> stringSupplier = () -> new String().intern();
+        Supplier<Object> objectSupplier = () -> new Object();
+        System.out.println("empty Object : " + sizer.estimateObjectSize(objectSupplier, sampleSize) + " bytes");
+
+        Supplier<Byte> byteSupplier = () -> new Byte("0");
+        System.out.println("Byte : " + sizer.estimateObjectSize(byteSupplier, sampleSize) + " bytes");
+
+        Supplier<Boolean> booleanSupplier = () -> new Boolean(false);
+        System.out.println("Boolean : " + sizer.estimateObjectSize(booleanSupplier, sampleSize) + " bytes");
+
+        Supplier<Short> shortSupplier = () -> new Short("0");
+        System.out.println("Short : " + sizer.estimateObjectSize(shortSupplier, sampleSize) + " bytes");
+
+        Supplier<Character> charSupplier = () -> new Character('a');
+        System.out.println("Character : " + sizer.estimateObjectSize(charSupplier, sampleSize) + " bytes");
+
+        Supplier<Integer> intSupplier = () -> new Integer(0);
+        System.out.println("Integer : " + sizer.estimateObjectSize(intSupplier, sampleSize) + " bytes");
+
+        Supplier<Float> floatSupplier = () -> new Float(0f);
+        System.out.println("Float : " + sizer.estimateObjectSize(floatSupplier, sampleSize) + " bytes");
+
+        Supplier<Long> longSupplier = () -> new Long(0L);
+        System.out.println("Long : " + sizer.estimateObjectSize(longSupplier, sampleSize) + " bytes");
+
+        Supplier<Double> doubleSupplier = () -> new Double(.0d);
+        System.out.println("Double : " + sizer.estimateObjectSize(doubleSupplier, sampleSize) + " bytes");
+        System.out.println();
+
+        Supplier<String> stringSupplier = () -> new String("A").intern();
         System.out.println("interned String : " + sizer.estimateObjectSize(stringSupplier, 1) + " bytes");
 
         stringSupplier = () -> new String();
         System.out.println("empty String : " + sizer.estimateObjectSize(stringSupplier, sampleSize) + " bytes");
+        System.out.println();
 
-        Supplier<Object> objectSupplier = () -> new Object();
-        System.out.println("empty Object : " + sizer.estimateObjectSize(objectSupplier, sampleSize) + " bytes");
-
-
-        Supplier<Object> intArray = () -> new int[0];
-        System.out.println("int[0] : " + sizer.estimateObjectSize(intArray, sampleSize) + " bytes");
-        intArray = () -> new int[1];
-        System.out.println("int[1] : " + sizer.estimateObjectSize(intArray, sampleSize) + " bytes");
-        intArray = () -> new int[2];
-        System.out.println("int[2] : " + sizer.estimateObjectSize(intArray, sampleSize) + " bytes");
-        intArray = () -> new int[3];
-        System.out.println("int[3] : " + sizer.estimateObjectSize(intArray, sampleSize) + " bytes");
+        Supplier<Object> intArraySupplier = () -> new int[0];
+        System.out.println("int[0] : " + sizer.estimateObjectSize(intArraySupplier, sampleSize) + " bytes");
+        intArraySupplier = () -> new int[1];
+        System.out.println("int[1] : " + sizer.estimateObjectSize(intArraySupplier, sampleSize) + " bytes");
+        intArraySupplier = () -> new int[2];
+        System.out.println("int[2] : " + sizer.estimateObjectSize(intArraySupplier, sampleSize) + " bytes");
+        intArraySupplier = () -> new int[3];
+        System.out.println("int[3] : " + sizer.estimateObjectSize(intArraySupplier, sampleSize) + " bytes");
     }
 }
